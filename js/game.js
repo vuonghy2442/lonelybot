cc8c55c7aa28f82c74dd12734664ac08e8f337fa
vuonghy2442 <vuonghy2442@hotmail.com.vn>
2024-02-25 10:25:36 +0700

    some feature

--- js/game.js
+++ js/game.js
@@ -1,19 +1,23 @@
+"use strict";
+
 window.onload = addListeners;
 
 const RANK_MAP = ['A', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 const SUIT_MAP = ['♡', '♢', '♧', '♤'];
 const N_SUITS = SUIT_MAP.length;
 const N_RANKS = RANK_MAP.length;
+const N_CARDS = N_SUITS * N_RANKS;
+
+const N_PILES = 7;
+const N_HIDDEN_CARDS = N_PILES * (N_PILES + 1) / 2;
+const N_FULL_DECK = N_CARDS - N_HIDDEN_CARDS;
+
 // ♤♡♢♧♠♥♦♣
 
-const game_box = document.querySelector("#game_box");
-
-function addListeners() {
-    document.querySelector("#deal").addEventListener('click', deal)
-    draggable(game_box);
+function cardId(rank, suit) {
+    return rank * N_SUITS + suit;
 }
 
-const cardMap = new Map();
 
 class Card {
     constructor(rank, suit) {
@@ -29,12 +33,11 @@ class Card {
 
 
         this.id = () => {
-            return this.rank * N_SUITS + this.suit;
+            return cardId(rank, suit);
         }
 
-        cardMap.set(this.id(), this); // Add card to map
-
         this.createDOM = (pos_x, pos_y) => {
+            if (this.element !== null) return;
             const c = document.createElement('div');
             c.id = `card_${rank}_${suit}`;
             c.className = suit < 2 ? "card red_card" : "card black_card";
@@ -69,7 +72,7 @@ class Card {
 
             c.style.left = pos_x + "%";
             c.style.top = pos_y + "%";
-            game_box.appendChild(c);
+            gameBox.appendChild(c);
             c.dataset.cardId = this.id();
 
             this.element = c;
@@ -86,11 +89,14 @@ class Card {
             let inner = this.element.firstElementChild;
             if (duration > 0)
                 inner.style.transition = `transform ${duration}ms`
+
             inner.classList.toggle("flipped");
-            if (duration > 0)
-                setTimeout(() => {
+
+            if (duration > 0) {
+                inner.addEventListener("transitionend", (e) => {
                     inner.style.removeProperty('transition');
-                }, duration)
+                }, { once: true })
+            }
         };
 
         this.moveTo = (pos_x, pos_y, duration) => {
@@ -108,47 +114,197 @@ class Card {
                 this.element.style.top = pos_y + "%";
 
             if (duration > 0) {
-                setTimeout(() => {
+                this.element.addEventListener("transitionend", (e) => {
                     this.element.style.removeProperty('transition');
                     this.animating = false;
-                }, duration);
+                }, { once: true });
             }
+
         }
 
-        this.is_draggable = () => {
+        this.isDraggable = () => {
             return this.draggable && !this.animating;
+        }
+
+        this.goBefore = (card) => {
+            return this.rank == card.rank + 1 && ((this.suit ^ card.suit) & 2 == 2 || this.rank == N_RANKS);
         }
     }
 }
 
+class Deck {
+    constructor(cards, draw_step) {
 
-function draggable(gameBox) {
+    }
+}
+
+const Pos = {
+    Deck: 0,
+    Stack: 1,
+    Pile: 1 + N_SUITS,
+}
+
+class Solitaire {
+    constructor(cards, draw_step) {
+        this.hidden_cards = cards.slice(0, N_HIDDEN_CARDS);
+        this.piles = Array.from(Array(N_PILES), (_, i) => {
+            return [this.hidden_cards[(i + 2) * (i + 1) / 2 - 1]];
+        });
+
+        this.deck = new Deck(cards.slice(N_HIDDEN_CARDS, N_CARDS), draw_step);
+        this.stack = Array.from(Array(N_SUITS), () => 0);
+
+        this.lift_card = (card) => {
+            // here
+            const res = new Array();
+            if (this.stack[card.suit] == card.rank) {
+                res.push(Pos.Stack + card.suit);
+            }
+
+            for (let i = 0; i < N_PILES; ++i) {
+                const p = this.piles[i];
+                if (p[p.length - 1].goBefore(card)) {
+                    res.push(Pos.Pile + i);
+                }
+            }
+
+            return res;
+        }
+    }
+};
+
+const gameBox = document.querySelector("#game_box");
+
+// creating cards
+const cardArray = (() => {
+    const cardArray = new Array(N_CARDS);
+
+    for (let rank = 0; rank < N_RANKS; ++rank) {
+        for (let suit = 0; suit < N_SUITS; ++suit) {
+            let c = new Card(rank, suit);
+            cardArray[c.id()] = c;
+        }
+    }
+    return cardArray;
+})();
+
+const game = new Solitaire(cardArray, 3);
+
+function addListeners() {
+    document.querySelector("#deal").addEventListener('click', deal)
+    initGame();
+}
+
+
+function getCard(rank, suit) {
+    return cardArray[cardId(rank, suit)];
+}
+
+const THRES = 0.06;
+const THRES_2 = THRES * THRES;
+
+var snap_audio = new Audio('sound/snap.mp3');
+
+
+function initGame() {
+    let gameBoxBound = gameBox.getBoundingClientRect();
+
+    function getDOMPos(el) {
+        let bound = el.getBoundingClientRect();
+        const x = (bound.left - gameBoxBound.left) / gameBoxBound.width;
+        const y = (bound.top - gameBoxBound.top) / gameBoxBound.height;
+        return [x, y];
+    }
+
+    const pilePos = function () {
+        let pos = new Array();
+        // not correct but whatever =))
+        pos.push(getDOMPos(document.querySelector("#deal")));
+
+        for (let s of document.querySelectorAll("#stack > div")) {
+            pos.push(getDOMPos(s));
+        }
+
+        for (let i = 0; i < N_PILES; ++i) {
+            pos.push([1, 1]);
+        }
+
+        return pos;
+    }();
+
+    console.log(pilePos);
+
+    window.addEventListener('resize', (_) => {
+        gameBoxBound = gameBox.getBoundingClientRect();
+    });
+
+
     gameBox.addEventListener('mousedown', (event) => {
         const cardDOM = event.target.closest('.card');
 
         if (!cardDOM) return;
-        const card = cardMap.get(parseInt(cardDOM.dataset.cardId));
+        const card = cardArray[parseInt(cardDOM.dataset.cardId)];
 
-        if (!card.is_draggable()) return;
+        if (!card.isDraggable()) return;
 
-        const gameBoxWidth = parseFloat(window.getComputedStyle(gameBox).width);
-        const gameBoxHeight = parseFloat(window.getComputedStyle(gameBox).height);
+        snap_audio.play();
 
-        const initialX = parseFloat(window.getComputedStyle(cardDOM).left) / gameBoxWidth;
-        const initialY = parseFloat(window.getComputedStyle(cardDOM).top) / gameBoxHeight;
 
-        const offsetX = event.clientX / gameBoxWidth - initialX;
-        const offsetY = event.clientY / gameBoxHeight - initialY;
+        const dropPos = game.lift_card(card);
+
+        const [initialX, initialY] = getDOMPos(cardDOM);
+
+        const offsetX = (event.clientX - gameBoxBound.left) / gameBoxBound.width - initialX;
+        const offsetY = (event.clientY - gameBoxBound.top) / gameBoxBound.height - initialY;
 
         let changed = false;
+        let snapped = -1;
+
+        function distance2(x, y, p) {
+            let [u, v] = pilePos[p];
+
+            const [dx, dy] = [x - u, y - v];
+            return dx * dx + dy * dy;
+        }
+
+        function findNear(x, y) {
+            if (snapped >= 0) return snapped;
+            for (let p of dropPos) {
+                if (distance2(x, y, p) < THRES_2) {
+                    return p;
+                }
+            }
+            return -1
+        }
 
         function handleMouseMove(event) {
-            const clientX = event.clientX / gameBoxWidth;
-            const clientY = event.clientY / gameBoxHeight;
+            if (!card.isDraggable()) return;
 
-            cardDOM.style.top = (clientY - offsetY) * 100 + '%';
-            cardDOM.style.left = (clientX - offsetX) * 100 + '%';
+            let x = (event.clientX - gameBoxBound.left) / gameBoxBound.width - offsetX;
+            let y = (event.clientY - gameBoxBound.top) / gameBoxBound.height - offsetY;
 
+            let p = findNear(x, y);
+
+            if (p >= 0) {
+                let [u, v] = pilePos[p];
+                const [dx, dy] = [x - u, y - v];
+                let dis2 = distance2(x, y, p);
+                let d = Math.max(Math.sqrt(dis2) / THRES - 1, 0);
+
+                if (d == 0 && snapped < 0) {
+                    snapped = p;
+                } else if (d > 0 && snapped >= 0) {
+                    snapped = -1;
+                }
+
+                const force = d > 0 ? 0 : 1;
+                x -= dx * force;
+                y -= dy * force;
+            }
+
+
+
+            card.moveTo(x * 100, y * 100, 0);
             changed = true;
         }
 
@@ -161,6 +317,8 @@ function draggable(gameBox) {
                 // Handle the case where the card wasn't dragged
                 card.flipCard(300);
             }
+
+            card.draggable = true;
         }
 
         window.addEventListener('mousemove', handleMouseMove);
@@ -170,17 +328,15 @@ function draggable(gameBox) {
 
 
 function deal() {
-
     let deal_card = (rank, suit, pos) => {
-
-        const c = new Card(rank, suit);
+        const c = getCard(rank, suit);
         c.flipCard();
         c.draggable = pos == 2;
         c.createDOM(2, 2.5);
 
         setTimeout(() => {
             c.flipCard(300);
-            c.moveTo(15 + pos * 2, 2.5, 1000);
+            c.moveTo(15 + pos * 2, 2.5, 300);
         }, 200 * pos);
     }
 
