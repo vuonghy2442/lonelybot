f92365c0b471c90dcb88ff58606b8f154c9df237
vuonghy2442 <vuonghy2442@hotmail.com.vn>
2024-09-27 19:46:05 +0700

    split file

--- js/game.js
+++ js/game.js
@@ -1,13 +1,8 @@
 "use strict";
 
-window.onload = addListeners;
+import { Card, N_CARDS, N_SUITS, N_RANKS, cardId } from "./Card.js";
 
-const RANK_MAP = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-const SUIT_MAP = ["♡", "♢", "♧", "♤"];
-const N_SUITS = SUIT_MAP.length;
-const N_RANKS = RANK_MAP.length;
-const KING_RANK = N_RANKS - 1;
-const N_CARDS = N_SUITS * N_RANKS;
+window.onload = addListeners;
 
 const N_PILES = 7;
 const N_HIDDEN_CARDS = (N_PILES * (N_PILES + 1)) / 2;
@@ -20,173 +15,6 @@ const DEAL_SPACE = 25;
 const ANIMATION_TIME = 100;
 const OFFSET_TIME = 100;
 const REVEAL_TIME = 300;
-
-// ♤♡♢♧♠♥♦♣
-
-function cardId(rank, suit) {
-  return rank * N_SUITS + suit;
-}
-
-function createCardSVG(c) {
-  const rank_str = RANK_MAP[c.rank];
-  const suit_str = SUIT_MAP[c.suit];
-  const color = c.suit < 2 ? "#e22" : "#001";
-
-  return `<svg xmlns="http://www.w3.org/2000/svg" class="card_front" fill="${color}" viewBox="0 0 250 350">
-    <g font-size="45">
-      <text x="5%" y="50">${rank_str}</text>
-      <text x="5%" y="95">${suit_str}</text>
-    </g>
-    <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="150">${rank_str}</text>
-    <g font-size="45" transform="rotate(180 125 175)">
-      <text x="5%" y="50">${rank_str}</text>
-      <text x="5%" y="95">${suit_str}</text>
-    </g>
-  </svg>`;
-}
-
-// const container = document.querySelector("#card_container");
-
-let front_element = null;
-class Card {
-  #rank;
-  #suit;
-  #flipped;
-  #draggable;
-  #animating;
-  #element;
-
-  constructor(rank, suit) {
-    if (typeof rank !== "number" || rank < 0 || rank > N_RANKS) throw new Error("Invalid rank");
-    if (typeof suit !== "number" || suit < 0 || suit >= N_SUITS) throw new Error("Invalid suit");
-
-    this.#rank = rank;
-    this.#suit = suit;
-    this.#flipped = false;
-    this.#draggable = true;
-    this.#animating = false;
-    this.#element = null;
-  }
-
-  get id() {
-    return cardId(this.#rank, this.#suit);
-  }
-
-  get rank() {
-    return this.#rank;
-  }
-
-  get suit() {
-    return this.#suit;
-  }
-
-  get animating() {
-    return this.#animating;
-  }
-
-  get element() {
-    return this.#element;
-  }
-
-  get container() {
-    return this.#element.parentElement;
-  }
-
-  set draggable(val) {
-    this.#draggable = val;
-  }
-
-  deleteDOM() {
-    if (this.#element === null) return;
-    this.#element.remove();
-    this.#element = null;
-  }
-
-  moveToFront() {
-    if (this.#element === null) return;
-    this.container.appendChild(this.#element);
-  }
-
-  containerToFront() {
-    if (this.#element === null) return;
-    if (front_element !== null) front_element.style.zIndex = 0;
-    this.container.style.zIndex = 1;
-    front_element = this.container;
-  }
-
-  createDOM(container, posX, posY) {
-    if (this.#element !== null) return;
-    const cardElement = document.createElement("div");
-    cardElement.className = "card";
-    cardElement.innerHTML = `<div class="card_inner">
-      <div class="card_back"></div>
-      ${createCardSVG(this)}
-    </div>`;
-    const inner = cardElement.firstElementChild;
-    if (this.#flipped) inner.classList.add("flipped");
-
-    cardElement.style.left = `${posX}%`;
-    cardElement.style.top = `${posY}%`;
-    cardElement.dataset.cardId = this.id;
-
-    cardElement.addEventListener("transitionrun", (_) => {
-      this.#animating = true;
-    });
-
-    const doneAnimate = (_) => {
-      cardElement.style.removeProperty("transition");
-      this.#animating = false;
-    };
-
-    cardElement.addEventListener("transitioncancel", doneAnimate);
-    cardElement.addEventListener("transitionend", doneAnimate);
-
-    container.appendChild(cardElement);
-    this.#element = cardElement;
-  }
-
-  turnUp(duration) {
-    if (this.#flipped) {
-      this.flipCard(duration);
-    }
-  }
-
-  flipCard(duration) {
-    this.#flipped = !this.#flipped;
-    if (this.#element === null) return;
-    const inner = this.#element.firstElementChild;
-    inner.classList.toggle("flipped");
-  }
-
-  moveTo(container, posX, posY, duration) {
-    if (this.#element === null) return;
-    const sameContainer = container === null || container === this.container;
-    let currentLeft = parseFloat(this.#element.style.left) || 0;
-    let currentTop = parseFloat(this.#element.style.top) || 0;
-
-    if (sameContainer && Math.abs(currentLeft - posX) < 1e-2 && Math.abs(currentTop - posY) < 1e-2) {
-      return;
-    }
-
-    if (duration > 0) this.#element.style.transition = `top ${duration}ms ease-in, left ${duration}ms ease-in`;
-    if (posX !== null) this.#element.style.left = `${posX}%`;
-    if (posY !== null) this.#element.style.top = `${posY}%`;
-    if (!sameContainer) {
-      const parent = this.container;
-      parent.removeChild(this.#element);
-      container.appendChild(this.#element);
-    }
-  }
-
-  isDraggable() {
-    return this.#draggable && !this.#animating;
-  }
-
-  goBefore(card) {
-    if (typeof card !== "object" || !(card instanceof Card)) throw new Error("Invalid card");
-    return this.#rank === card.#rank + 1 && (((this.#suit ^ card.#suit) & 2) === 2 || this.#rank === N_RANKS);
-  }
-}
 
 class Deck {
   constructor(cards, drawStep) {
@@ -455,25 +283,25 @@ function handlePushStackEvent(card) {
   card.moveToFront();
 
   if (card.rank >= 2) {
-    cardArray[cardId(card.rank - 2, card.suit)].deleteDOM();
+    getCard(card.rank - 2, card.suit).deleteDOM();
   }
 
   if (card.rank > 0) {
-    cardArray[cardId(card.rank - 1, card.suit)].draggable = false;
+    getCard(card.rank - 1, card.suit).draggable = false;
   }
 }
 
 function handlePopStackEvent(card) {
   if (card.rank >= 2) {
-    let c = cardArray[cardId(card.rank - 2, card.suit)];
+    let c = getCard(card.rank - 2, card.suit);
     c.turnUp();
     c.draggable = false;
     c.createDOM(stackConts[card.suit], 0, 0);
-    cardArray[cardId(card.rank - 1, card.suit)].moveToFront();
+    getCard(card.rank - 1, card.suit).moveToFront();
   }
 
   if (card.rank > 0) {
-    cardArray[cardId(card.rank - 1, card.suit)].draggable = true;
+    getCard(card.rank - 1, card.suit).draggable = true;
   }
 }
 
